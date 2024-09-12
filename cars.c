e68dc9f70f3b3d753c58f68beadf6f0e70550691
samuel <samuel.gomes.k276@gmail.com>
2024-09-12 18:57:20 +0100

    falta tudo de historicos e assim

--- cars.c
+++ cars.c
@@ -6,13 +6,13 @@
 #include "data.h"
 #include "data.c"
 
-#define LICENSESIZE 6
+#define LICENSESIZE 7
 #define PARKED 1
 
 struct carro {
     char license[LICENSESIZE];
     short isParked;
-    history history;
+    History history;
     TipoPrecos faturacao;  
 
     Car next;    
@@ -24,11 +24,38 @@ struct CarHistory {
     DataEHora exitTime;
     const char* parkName;
 
-    history next;
+    History next;
 };
+
+short carExists (Car AllCars, char license[6]) {
+    Car current = AllCars;
+    while (current != NULL) {
+        if (!strcmp(license, current->license)) {
+            return 1;
+        }
+        current = current->next;
+    }
+    return 0;
+}
 
 short isParked (Car thisCar) {
     return thisCar->isParked;
+}
+
+Car findCar (Car AllCars, char license[6]) {
+    Car current =  AllCars;
+    while (strcmp(current->license, license)) {
+        current = current->next;
+    }
+    return current;
+}
+
+History findHistory (History firstRegist) {
+    History current = firstRegist;
+    while (current != NULL && current->exitTime.data.ano != NULL) {
+        current = current->next;
+    }
+    return current;
 }
 
 Car createCar (char license[6]) {
@@ -40,19 +67,43 @@ Car createCar (char license[6]) {
     return thisCar;
 }
 
-void carEntry (Park AllParks, char *parkName, Car thisCar, DataEHora *ultimoRegisto) {
+void carEntry (Park AllParks, char *parkName, Car thisCar, DataEHora time) {
     Park thisPark = findPark(AllParks, parkName);
+    History newHistoric = findHistory()
+    thisCar->isParked = PARKED;
+    
 
 }
 
 
 
 void comandoE(Park AllParks, Car AllCars, DataEHora *ultimoRegisto) {
+    DataEHora time;
+    char license[LICENSESIZE], l1[3], l2[3], l3[3];
+    const char *parkName = lernome();
 
-    if (noSuchPark(AllParks, ...)) {
+    scanf("%s-%s-%s %d-%d-%d %d:%d", l1, l2, l3, &time.data.dia, &time.data.mes, &time.data.dia, &time.hora.horas, &time.hora.minutos);
+    strcpy(license, l1);
+    strcat(license, l2);
+    strcat(license, l3);
+
+    if (noSuchPark(AllParks, parkName)) {
 
         return;
     }
 
+    Car thisCar;
+    if (carExists(AllCars, license)) {
+        thisCar = findCar(AllCars, license);
+    } else {
+        thisCar = createCar(license);
+    }
 
+    if (isParked(thisCar)) {
+
+        return;
+    }
+    
+    carEntry(AllParks, parkName, thisCar, time);
+    changeDate(ultimoRegisto, time);
 }
--- cars.h
+++ cars.h
@@ -3,15 +3,19 @@
 
 
 typedef struct carro *Car;
-typedef struct CarHistory *history;
+typedef struct CarHistory *History;
 
 //verifications
 short isParked (Car thisCar);
+short carExists (Car AllCars, char license[6]);
 
 //constructors
 Car createCar (char license[6]);
-void carEntry (Park AllParks, char *parkName, Car thisCar, DataEHora *ultimoRegisto);
+void carEntry (Park AllParks, char *parkName, Car thisCar, DataEHora time);
 void comandoE(Park AllParks, Car AllCars, DataEHora *ultimoRegisto);
+
+//getters
+Car findCar (Car AllCars, char license[6]);
 
 
 #endif
