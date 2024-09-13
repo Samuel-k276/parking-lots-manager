9849bbe2242531c74f745f12c39c8fc04000be2e
samuel <samuel.gomes.k276@gmail.com>
2024-09-13 12:13:01 +0100

    +timeDiference()

--- data.h
+++ data.h
@@ -13,6 +13,8 @@ void changeDate (DataEHora *dateToChange, DataEHora newDate);
 short invalidDate (DataEHora date);
 short mostRecent (DataEHora data1, DataEHora data2);
 
-
+//getters
+int timeDiference (DataEHora recent, DataEHora old);
+short difDias (DataEHora recent, DataEHora old);
 
 #endif
